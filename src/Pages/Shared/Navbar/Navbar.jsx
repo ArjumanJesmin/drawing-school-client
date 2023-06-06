9d4013ba88f429e6d2506ee2d97f363f62dffe70
ArjumanJesmin <arjumanjesmin221186@gmail.com>
2023-06-07 00:43:55 +0600

    Updated routes and Created navbar and Footer

---
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import Container from "../Container/Container";
+
+const Navbar = () => {
+    return (
+        <Container>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <Link className="btn btn-ghost normal-case text-xl">Crown Art</Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><Link>Home</Link></li>
+                        <li><Link>Instructors</Link></li>
+                        <li><Link>Class</Link></li>
+                        <li><Link>Dashboard</Link></li>
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                <Link><button className="btn btn-outline btn-info">Login</button></Link>
+                </div>
+            </div>
+        </Container>
+    );
+};
+
+export default Navbar;
