9cf28e36de67600d1da86300dca04b6ce9281886
ArjumanJesmin <arjumanjesmin221186@gmail.com>
2023-06-09 23:48:15 +0600

    user deleted is done

--- src/Pages/Class/AllClass.jsx
+++ src/Pages/Class/AllClass.jsx
@@ -7,58 +7,61 @@ import { useNavigate } from "react-router-dom";
 
 const AllClass = ({ item }) => {
     const { name, image, price, rating } = item
-    const {user} = useContext(AuthContext)
-    const navigate =useNavigate()
+    const { user } = useContext(AuthContext)
+    const navigate = useNavigate()
 
-    if(user && user.email){
-        // const classItem = {name, image,price,email:user.email}
-        fetch('http://localhost:5000/classes',{
-            method:'POST',
-            headers:{
-                'content-type':'application/json'
-            },
-            body: JSON.stringify()
-        })
-        .then(res =>res.json())
-        .then(data =>{
-            if(data.insertedId){
-                Swal.fire({
-                    position: 'top-end',
-                    icon: 'success',
-                    title: 'Your work has been saved',
-                    showConfirmButton: false,
-                    timer: 1500
-                  })
-            }
-            else{
-                Swal.fire({
-                    title: 'Please Login',
-                    icon: 'warning',
-                    showCancelButton: true,
-                    confirmButtonColor: '#3085d6',
-                    cancelButtonColor: '#d33',
-                    confirmButtonText: 'Login new'
-                  }).then((result) => {
-                    if (result.isConfirmed) {
-                      navigate('/login')
+    const handleSelect = () => {
+        if (user && user.email) {
+            // const classItem = {name, image,price,email:user.email}
+            fetch('http://localhost:5000/classes', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify()
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.insertedId) {
+                        Swal.fire({
+                            position: 'top-end',
+                            icon: 'success',
+                            title: 'Your work has been saved',
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
                     }
-                  })
-            }
-        })
+                    else {
+                        Swal.fire({
+                            title: 'Please Login',
+                            icon: 'warning',
+                            showCancelButton: true,
+                            confirmButtonColor: '#3085d6',
+                            cancelButtonColor: '#d33',
+                            confirmButtonText: 'Login new'
+                        }).then((result) => {
+                            if (result.isConfirmed) {
+                                navigate('/login')
+                            }
+                        })
+                    }
+                })
+        }
     }
+
     return (
 
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img  src={image} alt="" /></figure>
-                <div className="card-body">
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src={image} alt="" /></figure>
+            <div className="card-body">
                 <h3 className="uppercase">{name}</h3>
-                    <p> Rating: {rating}</p>
-                    <p className="text-yellow-500 pb-4"> price: ${price}</p>
-                    <div className="card-actions justify-end">
-                    <button className="btn btn-outline ">Select</button>
-                    </div>
+                <p> Rating: {rating}</p>
+                <p className="text-yellow-500 pb-4"> price: ${price}</p>
+                <div className="card-actions justify-end">
+                    <button onClick={handleSelect} className="btn btn-outline ">Select</button>
                 </div>
             </div>
+        </div>
     );
 };
 
--- src/Pages/Dashboard/Admin/AllUsers.jsx
+++ src/Pages/Dashboard/Admin/AllUsers.jsx
@@ -2,38 +2,64 @@
 
 import { useQuery } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
-import { FaTrashAlt, FaUserShield } from "react-icons/fa";
+import { FaTrashAlt, FaUserAlt } from "react-icons/fa";
 import Swal from "sweetalert2";
 
 
 const AllUsers = () => {
-    const {data: users = [], refetch} = useQuery([],async()=>{
+    const { data: users = [], refetch } = useQuery([], async () => {
         const res = await fetch('http://localhost:5000/users')
         return res.json()
     })
 
-    const handleMakeAdmin = user =>{
+    const handleMakeAdmin = user => {
         fetch(`http://localhost:5000/users/admin/${user._id}`, {
             method: 'PATCH'
         })
-        .then(res => res.json())
-        .then(data => {
-            
-            if(data.modifiedCount){
-                refetch();
-                Swal.fire({
-                    position: 'top-end',
-                    icon: 'success',
-                    title: `${user.name} is an Admin Now!`,
-                    showConfirmButton: false,
-                    timer: 1500
-                  })
-            }
-        })
+            .then(res => res.json())
+            .then(data => {
+
+                if (data.modifiedCount) {
+                    refetch();
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: `${user.name} is an Admin Now!`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
+            })
     }
 
     const handleDelete = user => {
-
+        console.log(user);
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/users/admin/${user._id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'This user has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        })
     }
 
     return (
@@ -62,14 +88,14 @@ const AllUsers = () => {
                                 <td>{user.name}</td>
                                 <th><img src={user.image} alt="" /></th>
                                 <td>{user.email}</td>
-                                <td>{ user.role === 'admin' ? 'admin' :
-                                    <button onClick={() => handleMakeAdmin(user)} className="btn btn-ghost bg-lime-500 text-white"><FaUserShield></FaUserShield></button> 
-                                    }</td>
-                                <td><button onClick={() => handleDelete(user)} className="btn btn-ghost bg-red-600  text-white"><FaTrashAlt></FaTrashAlt></button></td>
+                                <td>{user.role === 'admin' ? 'admin' :
+                                    <button onClick={() => handleMakeAdmin(user)} className="btn   btn-primary"><FaUserAlt /></button>
+                                }</td>
+                                <td><button onClick={() => handleDelete(user)} className="btn btn-ghost bg-red-600  text-white"><FaTrashAlt /></button></td>
                             </tr>)
                         }
-                        
-                        
+
+
                     </tbody>
                 </table>
             </div>
