27de3cb0a76bbe5fefe3eb573fe70a672f0d44c0
ArjumanJesmin <arjumanjesmin221186@gmail.com>
2023-06-09 21:33:07 +0600

    see all user data

--- src/Pages/Dashboard/Admin/AllUsers.jsx
+++ src/Pages/Dashboard/Admin/AllUsers.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable no-unused-vars */
 
-import { useQuery} from '@tanstack/react-query'
+import { useQuery } from "@tanstack/react-query";
+import { Helmet } from "react-helmet-async";
+import { FaTrashAlt, FaUserShield } from "react-icons/fa";
+import Swal from "sweetalert2";
+
 
 const AllUsers = () => {
     const {data: users = [], refetch} = useQuery([],async()=>{
@@ -8,11 +12,67 @@ const AllUsers = () => {
         return res.json()
     })
 
-    return (
-        <div>
-            {
-                users.length
+    const handleMakeAdmin = user =>{
+        fetch(`http://localhost:5000/users/admin/${user._id}`, {
+            method: 'PATCH'
+        })
+        .then(res => res.json())
+        .then(data => {
+            
+            if(data.modifiedCount){
+                refetch();
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: `${user.name} is an Admin Now!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
             }
+        })
+    }
+
+    const handleDelete = user => {
+
+    }
+
+    return (
+        <div className="w-full">
+            <Helmet>
+                <title>Akibuki | All users</title>
+            </Helmet>
+            <h3 className="text-3xl font-semibold my-4">Total Users: {users.length}</h3>
+            <div className="overflow-x-auto">
+                <table className="table table-zebra w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Image</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index) => <tr key={user._id}>
+                                <th>{index + 1}</th>
+                                <td>{user.name}</td>
+                                <th><img src={user.image} alt="" /></th>
+                                <td>{user.email}</td>
+                                <td>{ user.role === 'admin' ? 'admin' :
+                                    <button onClick={() => handleMakeAdmin(user)} className="btn btn-ghost bg-lime-500 text-white"><FaUserShield></FaUserShield></button> 
+                                    }</td>
+                                <td><button onClick={() => handleDelete(user)} className="btn btn-ghost bg-red-600  text-white"><FaTrashAlt></FaTrashAlt></button></td>
+                            </tr>)
+                        }
+                        
+                        
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -5,19 +5,19 @@ import router from './Routes/Routes.jsx'
 import { RouterProvider } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './providers/AuthProvider'
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      {/* <QueryClientProvider client={QueryClient}> */}
-      <HelmetProvider>
-        <div className='max-w-screen-xl mx-auto'>
-          <RouterProvider router={router} />
-        </div>
-      </HelmetProvider>
-    {/* </QueryClientProvider> */}
+      <QueryClientProvider client={queryClient}>
+        <HelmetProvider>
+          <div className='max-w-screen-xl mx-auto'>
+            <RouterProvider router={router} />
+          </div>
+        </HelmetProvider>
+      </QueryClientProvider>
     </AuthProvider>
   </React.StrictMode>,
 )
